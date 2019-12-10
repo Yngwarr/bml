eeba82a43ae570065c2baecac0c828439e20463b
Yngwarr <k.yngvarr@gmail.com>
2019-12-10 12:36:13 +0700

    Methodology's up

---
+++ methodology.js
@@ -0,0 +1,35 @@
+function slurp(name, qid) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            ready(this.responseText, qid);
+        }
+        console.error("Couldn't slurp a test page. :(");
+    };
+    xhttp.open("GET", name, true);
+    xhttp.send();
+}
+
+function ready(txt, qid) {
+    let page = document.createElement('html');
+    page.innerHTML = txt;
+
+    let ids = [];
+    page.querySelectorAll('.correct_response_offline').forEach(
+        e => ids.push(e.parentElement.getAttribute('for')));
+    
+    ids.filter(id => id.includes(qid))
+        .forEach(id => document.getElementById(id).click);
+}
+
+function main() {
+    const url = document.URL.split('/');
+    const tid = url[url.length - 2];
+    const qid = url[url.length - 1];
+    slurp(
+        `https://dispace.edu.nstu.ru/ditest/index/offline/html/${tid}?format=only_data&control=add_close&score=show_full`,
+        qid
+    );
+}
+
+main();
